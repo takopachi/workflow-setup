393cdb61bdcbe3c909e989943095514fc345ffb2
takopachi <songlip.lim@redalphacyber.com>
2025-08-06 11:20:16 +0800

    Added main.py, test.py and updated build-and-release.yml

---
+++ main.py
@@ -0,0 +1,5 @@
+def add(a, b):
+    return a + b
+
+def test():
+    assert add(3, 5) == 1  # Failing test on purpose
---
+++ test.py
@@ -0,0 +1,5 @@
+from main import test
+
+if __name__ == "__main__":
+    test()
+    print("All tests passed.")
